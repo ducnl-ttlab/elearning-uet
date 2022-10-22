e1cfafc81228193afce89d9789391540ca70d4fc
ducnl <ducnl@tokyotechlab.com>
2022-10-23 05:52:08 +0700

    add file image public

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -7,28 +7,52 @@ import {
   UploadedFile,
   Post,
   BadRequestException,
+  ParseIntPipe,
+  StreamableFile,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { AppService } from './app.service';
-import { extname } from 'path';
-import { diskStorage } from 'multer';
+import { extname, join } from 'path';
+import { Response } from 'express';
+
 import LocalFilesInterceptor, {
-  imageFilter,
   mediaParams,
 } from 'src/infra/local-file/local-files.interceptor';
 import { ApiBody, ApiConsumes } from '@nestjs/swagger';
+import { createReadStream } from 'fs';
 
-@Controller('image')
+@Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  @Get('image/category/:name')
+  Category(
+    @Param('name') name: string,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    const stream = createReadStream(join(process.cwd(), `/public/${name}.png`));
 
-  // @Get(':name')
-  // getImage(@Param('name') name, @Res() res): void {
-  //   res.sendFile(name, { root: 'public' });
-  // }
-  @Get('')
-  getHelloWold(@Res() res): void {
-    res.send('hello world');
+    response.set({
+      'Content-Disposition': `inline; filename="${name}"`,
+      'Content-Type': 'image/jpeg',
+    });
+
+    return new StreamableFile(stream);
+  }
+
+  @Get('files/:id')
+  getFiles(
+    @Param('id') id: string,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    const stream = createReadStream(
+      join(process.cwd(), `/uploads/avatar/1666434020416-994288424.jpeg`),
+    );
+
+    response.set({
+      'Content-Disposition': `inline; filename="${id}"`,
+      'Content-Type': 'image/jpeg',
+    });
+
+    return new StreamableFile(stream);
   }
 
   @Post('avatar')
