d06dca4ce2d2eae34354dcf204da1938fd0554e8
ducnl <ducnl@tokyotechlab.com>
2022-10-27 22:14:37 +0700

    remove file unuse

--- backend/src/modules/course/service/course.service.ts
+++ backend/src/modules/course/service/course.service.ts
@@ -2,7 +2,6 @@ import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Course } from '../entity/course.entity';
-import { CourseDto } from '../dto/course.dto';
 
 @Injectable()
 export class CourseService {
