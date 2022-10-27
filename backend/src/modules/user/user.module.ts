54fe3eb426e67aee56d9d0c329650dda2f66c97e
ducnl <ducnl@tokyotechlab.com>
2022-10-28 01:09:29 +0700

    add profile api

--- backend/src/modules/user/user.module.ts
+++ backend/src/modules/user/user.module.ts
@@ -1,11 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './service/user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), AuthModule],
   providers: [UserService],
   controllers: [UserController],
   exports: [UserService],
