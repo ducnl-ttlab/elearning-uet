cfff5a3b78ffe1394ef0e53eac449ba24c5323ac
ducnl <ducnl@tokyotechlab.com>
2022-10-27 22:36:20 +0700

    import category module

--- backend/src/modules/course/course.module.ts
+++ backend/src/modules/course/course.module.ts
@@ -3,9 +3,10 @@ import { CourseController } from './course.controller';
 import { CourseService } from './service/course.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Course } from './entity/course.entity';
+import { CategoryModule } from '../category/category.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Course])],
+  imports: [TypeOrmModule.forFeature([Course]), CategoryModule],
   providers: [CourseService],
   controllers: [CourseController],
   exports: [CourseService],
