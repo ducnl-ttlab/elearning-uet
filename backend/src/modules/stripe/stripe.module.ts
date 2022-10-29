db98cbd08de7fef0a3ec97c11326ecaad9a11058
ducnl <ducnl@tokyotechlab.com>
2022-10-29 23:53:30 +0700

    Merge branch 'main' of https://github.com/ducnl-ttlab/elearning-uet

---
+++ backend/src/modules/stripe/stripe.module.ts
@@ -0,0 +1,11 @@
+import { Module, forwardRef } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../auth/auth.module';
+
+@Module({
+  imports: [],
+  providers: [],
+  controllers: [],
+  exports: [],
+})
+export class UserModule {}
