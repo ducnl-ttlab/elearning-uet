c6e176c30af9d66ba4fc933d960df1230c1d8324
Bùi Xuân Hiếu <66052667+bxhieu713@users.noreply.github.com>
2022-11-12 11:03:20 +0700

    update

--- frontend/src/modules/homepage/pages/CategoryPage.vue
+++ frontend/src/modules/homepage/pages/CategoryPage.vue
@@ -34,7 +34,6 @@
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import {State, Action, Getter} from 'vuex-class';
 
 @Options({
     components: {},
@@ -45,15 +44,70 @@ export default class CategoryPage extends Vue {
     }
 
     courses = [
-        {
-            id: 1,
-            name: 'Java',
-            image: 'https://i.imgur.com/9ZQZ9Zu.jpg',
-            courseTotal: 10,
-            studentTotal: 100,
-            avgRating: 4.5,
-
-        }
+      {
+        "id": 1,
+        "name": "IT và phần mềm",
+        "image": "http://139.59.123.89:5000/category/image/software",
+        "avgRating": 5,
+        "courseTotal": 0,
+        "studentTotal": 0
+      },
+      {
+        "id": 2,
+        "name": "Kinh doanh",
+        "image": "http://139.59.123.89:5000/category/image/business",
+        "avgRating": 5,
+        "courseTotal": 1,
+        "studentTotal": 2
+      },
+      {
+        "id": 3,
+        "name": "sáng tạo",
+        "image": "http://139.59.123.89:5000/category/image/design",
+        "avgRating": 5,
+        "courseTotal": 2,
+        "studentTotal": 4
+      },
+      {
+        "id": 4,
+        "name": "Phát triển bản thân",
+        "image": "http://139.59.123.89:5000/category/image/personal-development",
+        "avgRating": 5,
+        "courseTotal": 3,
+        "studentTotal": 6
+      },
+      {
+        "id": 5,
+        "name": "Kinh doanh",
+        "image": "http://139.59.123.89:5000/category/image/marketing",
+        "avgRating": 5,
+        "courseTotal": 4,
+        "studentTotal": 8
+      },
+      {
+        "id": 6,
+        "name": "Lập trình",
+        "image": "http://139.59.123.89:5000/category/image/development",
+        "avgRating": 5,
+        "courseTotal": 5,
+        "studentTotal": 10
+      },
+      {
+        "id": 7,
+        "name": "Nghệ thuật",
+        "image": "http://139.59.123.89:5000/category/image/music",
+        "avgRating": 5,
+        "courseTotal": 6,
+        "studentTotal": 12
+      },
+      {
+        "id": 8,
+        "name": "Nhiếp ảnh",
+        "image": "http://139.59.123.89:5000/category/image/photography",
+        "avgRating": 5,
+        "courseTotal": 7,
+        "studentTotal": 14
+      }
     ];
 
 
