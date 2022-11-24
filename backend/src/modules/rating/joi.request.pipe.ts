fa210301f18f6e9fa92d024de751628f343c643e
duc nong <ducnl@tokyotechlab.com>
2022-11-24 19:49:15 +0700

    refactor code

--- backend/src/modules/rating/joi.request.pipe.ts
+++ backend/src/modules/rating/joi.request.pipe.ts
@@ -4,46 +4,20 @@ import {
   ValidationPipe,
 } from 'src/common/pipe/joi.request.pipe';
 
-
 const ratingBodySchema = Joi.object().keys({
   rating: Joi.string()
-  .pattern(/^[1-5]{1}/).message('rating should be a string'),
+    .pattern(/^[1-5]{1}/)
+    .message('rating should be a string'),
 });
 
-const topicQuerySchema = Joi.object().keys({
-  topicId: Joi.string()
-    .pattern(/^[0-9]+$/)
-    .message('topicId should be a number'),
-});
 const ratingParamSchema = Joi.object().keys({
   courseId: Joi.string()
     .pattern(/^[0-9]+$/)
     .message('topicId should be a number'),
 });
 
-const queryListSchema = Joi.object().keys({
-  topicId: Joi.string()
-    .pattern(/^[0-9]+$/)
-    .message('topicId should be a number'),
-  page: Joi.string()
-    .pattern(/^[0-9]+$/)
-    .message('page should be a number')
-    .optional(),
-  pageSize: Joi.string()
-    .pattern(/^[0-9]+$/)
-    .message('pageSize should be a number')
-    .optional(),
-});
-const deleteCommentParamSchema = Joi.object().keys({
-  courseId: Joi.string()
-    .pattern(/^[0-9]+$/)
-    .message('topicId should be a number'),
-  commentId: Joi.string()
-    .pattern(/^[0-9]+$/)
-    .message('topicId should be a number'),
-});
 const validationSchemas = {
-   ratingBodySchema,
+  ratingBodySchema,
   ratingParamSchema,
 };
 
