db842b6d407253f974ecde634268e45f68e4f8eb
minhan012710 <54834613+minhan012710@users.noreply.github.com>
2022-11-26 13:36:47 +0700

    add data

--- backend/database/seedings/1667010171189-Course.ts
+++ backend/database/seedings/1667010171189-Course.ts
@@ -13,6 +13,69 @@ export class Course1667010171189 implements MigrationInterface {
         isPublished: true,
         image: 'https://lptech.asia/uploads/files/2020/05/25/lap-trinh-web-php-mysql.png'
       },
+      {
+        instructorId: '19020281',
+        categoryId: 2,
+        name: 'khoa hoc lap trinh co ban',
+        description: 'khoa hoc lap trinh co ban',
+        price: 12321.1,
+        isPublished: true,
+        image: 'https://lptech.asia/uploads/files/2020/05/25/lap-trinh-web-php-mysql.png'
+      },
+      {
+        instructorId: '19020286',
+        categoryId: 3,
+        name: 'khoa hoc lap trinh co ban',
+        description: 'khoa hoc lap trinh co ban',
+        price: 12321.1,
+        isPublished: true,
+        image: 'https://lptech.asia/uploads/files/2020/05/25/lap-trinh-web-php-mysql.png'
+      },
+      {
+        instructorId: '19020075',
+        categoryId: 4,
+        name: 'khoa hoc lap trinh co ban',
+        description: 'khoa hoc lap trinh co ban',
+        price: 12321.1,
+        isPublished: true,
+        image: 'https://lptech.asia/uploads/files/2020/05/25/lap-trinh-web-php-mysql.png'
+      },
+      {
+        instructorId: '19020211',
+        categoryId: 5,
+        name: 'khoa hoc lap trinh co ban',
+        description: 'khoa hoc lap trinh co ban',
+        price: 12321.1,
+        isPublished: true,
+        image: 'https://lptech.asia/uploads/files/2020/05/25/lap-trinh-web-php-mysql.png'
+      },
+      {
+        instructorId: '123456xsabf',
+        categoryId: 6,
+        name: 'khoa hoc lap trinh co ban',
+        description: 'khoa hoc lap trinh co ban',
+        price: 12321.1,
+        isPublished: true,
+        image: 'https://lptech.asia/uploads/files/2020/05/25/lap-trinh-web-php-mysql.png'
+      },
+      {
+        instructorId: '123456abf',
+        categoryId: 7,
+        name: 'khoa hoc lap trinh co ban',
+        description: 'khoa hoc lap trinh co ban',
+        price: 12321.1,
+        isPublished: true,
+        image: 'https://lptech.asia/uploads/files/2020/05/25/lap-trinh-web-php-mysql.png'
+      },
+      {
+        instructorId: '19020476',
+        categoryId: 8,
+        name: 'khoa hoc lap trinh co ban',
+        description: 'khoa hoc lap trinh co ban',
+        price: 12321.1,
+        isPublished: true,
+        image: 'https://lptech.asia/uploads/files/2020/05/25/lap-trinh-web-php-mysql.png'
+      },
     ];
     let itemDatas = items.map((item) => {
       return {
--- backend/database/seedings/1669059281163-UserCourses.ts
+++ backend/database/seedings/1669059281163-UserCourses.ts
@@ -13,7 +13,7 @@ export class UserCourses1669059281163 implements MigrationInterface {
       {
         courseId: 1,
         userId: '123456xsabf',
-        status: UserCourseStatus.expired,
+        status: UserCourseStatus.,
         startCourseTime: new Date(),
       },
       {
@@ -27,9 +27,58 @@ export class UserCourses1669059281163 implements MigrationInterface {
       {
         courseId: 1,
         userId: '123456xsabxfxa',
+        status: UserCourseStatus.expired,
+        startCourseTime: new Date(),
+      },
+      {
+        courseId: 1,
+        userId: '19020356',
+        status: UserCourseStatus.pending,
+        startCourseTime: new Date(),
+      },
+      {
+        courseId: 3,
+        userId: '19020301',
         status: UserCourseStatus.accepted,
         startCourseTime: new Date(),
       },
+      {
+        courseId: 2,
+        userId: '19020080',
+        status: UserCourseStatus.accepted,
+        startCourseTime: new Date(),
+      },
+      {
+        courseId: 3,
+        userId: '19020346',
+        status: UserCourseStatus.accepted,
+        startCourseTime: new Date(),
+      },
+      {
+        courseId: 2,
+        userId: '19020166',
+        status: UserCourseStatus.accepted,
+        startCourseTime: new Date(),
+      },
+      {
+        courseId: 3,
+        userId: '19020361',
+        status: UserCourseStatus.accepted,
+        startCourseTime: new Date(),
+      },
+      {
+        courseId: 4,
+        userId: '19020371',
+        status: UserCourseStatus.accepted,
+        startCourseTime: new Date(),
+      },
+      {
+        courseId: 4,
+        userId: '19020321',
+        status: UserCourseStatus.accepted,
+        startCourseTime: new Date(),
+      },
+      
     ];
 
     await queryRunner.manager.getRepository(TableName.userCourse).insert(items);
