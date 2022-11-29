0d6ec242a007dba3ff7b5deb8fc9b200ba4574a3
hieudt <hieudt@tokyotechlab.com>
2022-11-29 21:08:37 +0700

    temp

--- frontend/src/modules/auth/services/login.ts
+++ frontend/src/modules/auth/services/login.ts
@@ -1,28 +1,18 @@
-import { AxiosDefaultResponse } from '@/common/interfaces';
+import { IAxiosDefaultResponse } from '@/common/interfaces';
 import axios from 'axios';
 import { IGoogleLoginParams, IUserData } from '../constants/auth.interfaces';
 
 const FE_URL = process.env.VUE_APP_FE_BASE_URL;
 const BE_URL = process.env.VUE_APP_API_URL;
 
-export interface ILoginResponse {
-    code?: number;
-    message?: string;
-    success?: boolean;
-    errors?: Array<IErrorMessage>;
-    data?: {
-        accessToken?: string;
-        user: IUserData;
-    };
+export interface ILoginData {
+    accessToken?: string;
+    user: IUserData;
 }
 
-export interface IErrorMessage {
-    key?: string;
-    errorCode?: number;
-    message?: string;
-}
-
-export async function login(params: IGoogleLoginParams): Promise<ILoginResponse> {
+export async function login(
+    params: IGoogleLoginParams,
+): Promise<IAxiosDefaultResponse<ILoginData>> {
     const response = await axios.post(`${BE_URL}/auth/login`, params);
     return response.data;
 }
@@ -30,7 +20,7 @@ export async function login(params: IGoogleLoginParams): Promise<ILoginResponse>
 export async function selectRole(
     role: string,
     token: string,
-): Promise<AxiosDefaultResponse> {
+): Promise<IAxiosDefaultResponse<Record<string, never>>> {
     const response = await axios.put(
         `${BE_URL}/auth/select-role`,
         {
@@ -42,5 +32,5 @@ export async function selectRole(
             },
         },
     );
-    return response;
+    return response.data;
 }
--- frontend/src/modules/auth/services/register.ts
+++ frontend/src/modules/auth/services/register.ts
@@ -1,41 +1,59 @@
-import { AxiosDefaultResponse } from '@/common/interfaces';
+import { IAxiosDefaultResponse } from '@/common/interfaces';
 import axios from 'axios';
+import { IUserData } from '../constants/auth.interfaces';
 
 const FE_URL = process.env.VUE_APP_FE_BASE_URL;
 const BE_URL = process.env.VUE_APP_API_URL;
 
-export async function signupWithGoogle(email: string): Promise<AxiosDefaultResponse> {
+export async function signupWithGoogle(
+    email: string,
+): Promise<IAxiosDefaultResponse<string>> {
     const response = await axios.post(`${BE_URL}/auth/signup`, {
         url: `${FE_URL}/create-password/`,
         email: email,
     });
-    return response;
+    return response.data;
 }
 
-export async function verifyToken(token: string): Promise<AxiosDefaultResponse> {
+export async function verifyToken(
+    token: string,
+): Promise<IAxiosDefaultResponse<IUserData>> {
     const response = await axios.get(`${BE_URL}/auth/verify-email/${token}`);
-    return response;
+    return response.data;
 }
 
-export async function setPassword(password: string, token: string): Promise<AxiosDefaultResponse> {
-    const response = await axios.post(`${BE_URL}/auth/change-password`, {
-        password: password,
-    }, {
-        headers: {
-          Authorization: 'Bearer ' + token
-        }
-      });
-    return response;
+export async function setPassword(
+    password: string,
+    token: string,
+): Promise<IAxiosDefaultResponse<Record<string, unknown>>> {
+    const response = await axios.post(
+        `${BE_URL}/auth/change-password`,
+        {
+            password: password,
+        },
+        {
+            headers: {
+                Authorization: 'Bearer ' + token,
+            },
+        },
+    );
+    return response.data;
 }
 
-
-export async function selectRole(role: string, token: string): Promise<AxiosDefaultResponse> {
-    const response = await axios.put(`${BE_URL}/auth/select-role`, {
-        role: role,
-    }, {
-        headers: {
-          Authorization: 'Bearer ' + token
-        }
-      });
-    return response;
+export async function selectRole(
+    role: string,
+    token: string,
+): Promise<IAxiosDefaultResponse<Record<string, unknown>>> {
+    const response = await axios.put(
+        `${BE_URL}/auth/select-role`,
+        {
+            role: role,
+        },
+        {
+            headers: {
+                Authorization: 'Bearer ' + token,
+            },
+        },
+    );
+    return response.data;
 }
