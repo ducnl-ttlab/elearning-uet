8eb37997b0e8dc1ce87ffb542906069dd71b6f27
ducnl <ducnl@tokyotechlab.com>
2022-12-02 22:37:21 +0700

    update user api

--- backend/src/modules/user/user.module.ts
+++ backend/src/modules/user/user.module.ts
@@ -1,4 +1,5 @@
-import { Module, forwardRef } from '@nestjs/common';
+import { RedisCacheModule } from './../cache/redis-cache.module';
+import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './service/user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -7,7 +8,7 @@ import { AuthModule } from '../auth/auth.module';
 import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), AuthModule],
+  imports: [TypeOrmModule.forFeature([User]), AuthModule, RedisCacheModule],
   providers: [UserService, ConfigService],
   controllers: [UserController],
   exports: [UserService],
