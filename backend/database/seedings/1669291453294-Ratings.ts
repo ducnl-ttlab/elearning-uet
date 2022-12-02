52a35132324d22e7d6c9615d3b6407d219871423
minhan012710 <54834613+minhan012710@users.noreply.github.com>
2022-12-02 21:29:48 +0700

    seed-ratting

--- backend/database/seedings/1669291453294-Ratings.ts
+++ backend/database/seedings/1669291453294-Ratings.ts
@@ -7,6 +7,42 @@ export class Ratings1669291453294 implements MigrationInterface {
         userCourseId: 1,
         rating: '1',
       },
+      {
+        userCourseId: 2,
+        rating: '3',
+      },
+      {
+        userCourseId: 3,
+        rating: '4',
+      },
+      {
+        userCourseId: 4,
+        rating: '5',
+      },
+      {
+        userCourseId: 6,
+        rating: '2',
+      },
+      {
+        userCourseId: 8,
+        rating: '4',
+      },
+      {
+        userCourseId: 9,
+        rating: '2',
+      },
+      {
+        userCourseId: 10,
+        rating: '5',
+      },
+      {
+        userCourseId: 11,
+        rating: '5',
+      },
+      {
+        userCourseId: 12,
+        rating: '5',
+      },
     ];
 
     await queryRunner.manager.getRepository(TableName.rating).insert(items);
