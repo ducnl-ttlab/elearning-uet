ae8a994243fe82ece785f9b518c935d256aad9c1
hieudt <hieudt@tokyotechlab.com>
2022-12-03 09:26:40 +0700

    copy main

--- frontend/src/modules/auth/store/login.store.ts
+++ frontend/src/modules/auth/store/login.store.ts
@@ -1,21 +1,21 @@
 import { Module, VuexModule, Action, Mutation, getModule } from 'vuex-module-decorators';
 import store from '@/plugins/vuex/index';
-import { IUserInfo } from '@/modules/user/constants/user.interfaces';
+import { IUserData } from '../constants/auth.interfaces';
 
 @Module({ dynamic: true, namespaced: true, store, name: 'authLogin' })
 class LoginModule extends VuexModule {
     isLoggedIn = false;
     authProvider = '';
-    loginCredential: IUserInfo = {};
+    loginCredential: IUserData = {};
     accessToken = '';
 
     @Action
-    setLoginCredential(value: IUserInfo) {
+    setLoginCredential(value: IUserData) {
         this.SET_LOGIN_CREDENTIAL(value);
     }
 
     @Mutation
-    SET_LOGIN_CREDENTIAL(value: IUserInfo) {
+    SET_LOGIN_CREDENTIAL(value: IUserData) {
         this.loginCredential = value;
     }
 
