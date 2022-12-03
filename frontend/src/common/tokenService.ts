bcb58dda9c2ae8db7162093ca552fa08d354dab9
hieudt <hieudt@tokyotechlab.com>
2022-12-03 09:34:06 +0700

    copy main

--- frontend/src/common/tokenService.ts
+++ frontend/src/common/tokenService.ts
@@ -1,6 +1,14 @@
 import { DEFAULT_LANGUAGE, SupportLanguage } from '@/common/constants';
-import { IUserInfo } from '@/modules/user/constants/user.interfaces';
 import { storage } from './localStorage';
+
+export interface IUser {
+    id?: number;
+    firstName?: string;
+    lastName?: string;
+    password?: string;
+    email?: string;
+    phoneNumber?: string;
+}
 
 export const enum AUTH_SERVICE_KEY {
     ACCESS_TOKEN = 'ACCESS_TOKEN',
@@ -64,10 +72,10 @@ class LocalStorageTokenService {
     }
 
     // LOGIN USER
-    setLoginUser(user: IUserInfo): void {
+    setLoginUser(user: IUser): void {
         storage.setLocalStorage(AUTH_SERVICE_KEY.LOGIN_USER, JSON.stringify(user || ''));
     }
-    getLoginUser(): IUserInfo | null {
+    getLoginUser(): IUser | null {
         return storage.getLocalStorage(AUTH_SERVICE_KEY.LOGIN_USER)
             ? JSON.parse(storage.getLocalStorage(AUTH_SERVICE_KEY.LOGIN_USER))
             : null;
