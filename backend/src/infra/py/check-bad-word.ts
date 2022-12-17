1a014a11559f01bead1b47d30da0cc2ed7d167c0
hieudt <hieudt@tokyotechlab.com>
2022-12-18 03:52:58 +0700

    neural network

--- backend/src/infra/py/check-bad-word.ts
+++ backend/src/infra/py/check-bad-word.ts
@@ -5,12 +5,12 @@ export async function checkBadWord(comment: string): Promise<boolean> {
   let commentData = comment.replace(/\s/g, '_');
   let checkBadWordScriptPath = join(process.cwd(), '/src/infra/py');
 
-  let { stdout } = await exec(`python3 model.py ${commentData}`, {
+  let { stdout } = await exec(`python model.py ${commentData}`, {
     cwd: checkBadWordScriptPath,
   });
   return new Promise((resolve) => {
     stdout.on('data', (data) => {
-      let isBad = data.toString().toUpperCase().includes('1.0');
+      let isBad = data.toString().toUpperCase().includes('1');
       resolve(isBad);
     });
   });
